2e90dc91321c53f60e7e84352e03db118ec2bbe6
CD-Z <69157453+CD-Z@users.noreply.github.com>
2025-08-05 22:05:34 +0200

    added NovelLastReadContext

--- src/hooks/persisted/index.ts
+++ src/hooks/persisted/index.ts
@@ -11,7 +11,11 @@ export {
 } from './useSettings';
 export { default as usePlugins } from './usePlugins';
 export { getTracker, useTracker } from './useTracker';
-export { useNovel } from './novel/useNovel';
+export { default as useNovelChapters } from './novel/useNovelChapters';
+export { default as useNovelPages } from './novel/useNovelPages';
+export { default as useNovelSettings } from './novel/useNovelSettings';
+export { default as useNovelState } from './novel/useNovelState';
+export { default as useNovelLastRead } from './novel/useNovelLastRead';
 export { useTrackedNovel } from './novel/useTrackedNovel';
 export { default as useDownload } from './useDownload';
 export { default as useUserAgent } from './useUserAgent';
