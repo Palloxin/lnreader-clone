1ce16dc643cb4524109bdfc0761d3f49a53e5a99
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2025-12-06 12:56:51 +0530

    chore: Cleanup Unused Imports

--- src/screens/settings/SettingsReaderScreen/tabs/AccessibilityTab.tsx
+++ src/screens/settings/SettingsReaderScreen/tabs/AccessibilityTab.tsx
@@ -1,20 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { BottomSheetScrollView } from '@gorhom/bottom-sheet';
-import Slider from '@react-native-community/slider';
-import { getAvailableVoicesAsync, Voice } from 'expo-speech';
-import {
-  useTheme,
-  useChapterGeneralSettings,
-  useChapterReaderSettings,
-} from '@hooks/persisted';
+import { useChapterGeneralSettings, useTheme } from '@hooks/persisted';
 import { getString } from '@strings/translations';
-import { List, Button } from '@components/index';
+import { List } from '@components/index';
 import SettingSwitch from '../../components/SettingSwitch';
-import Switch from '@components/Switch/Switch';
-import { useBoolean } from '@hooks';
-import { Portal } from 'react-native-paper';
-import VoicePickerModal from '../Modals/VoicePickerModal';
 
 const AccessibilityTab: React.FC = () => {
   const theme = useTheme();
@@ -28,67 +18,67 @@ const AccessibilityTab: React.FC = () => {
   } = useChapterGeneralSettings();
 
   return (
-      <BottomSheetScrollView
-        style={styles.container}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.contentContainer}
-      >
-        <View style={styles.section}>
-          <List.SubHeader theme={theme}>
-            {getString('common.display')}
-          </List.SubHeader>
-          <SettingSwitch
-            label={getString('readerScreen.bottomSheet.fullscreen')}
-            value={fullScreenMode}
-            onPress={() =>
-              setChapterGeneralSettings({ fullScreenMode: !fullScreenMode })
-            }
-            theme={theme}
-          />
-          <SettingSwitch
-            label={getString('readerScreen.bottomSheet.showProgressPercentage')}
-            value={showScrollPercentage}
-            onPress={() =>
-              setChapterGeneralSettings({
-                showScrollPercentage: !showScrollPercentage,
-              })
-            }
-            theme={theme}
-          />
-          <SettingSwitch
-            label={getString('readerScreen.bottomSheet.showBatteryAndTime')}
-            value={showBatteryAndTime}
-            onPress={() =>
-              setChapterGeneralSettings({
-                showBatteryAndTime: !showBatteryAndTime,
-              })
-            }
-            theme={theme}
-          />
-          <SettingSwitch
-            label={getString('readerScreen.bottomSheet.keepScreenOn')}
-            value={keepScreenOn}
-            onPress={() =>
-              setChapterGeneralSettings({ keepScreenOn: !keepScreenOn })
-            }
-            theme={theme}
-          />
-        </View>
+    <BottomSheetScrollView
+      style={styles.container}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.contentContainer}
+    >
+      <View style={styles.section}>
+        <List.SubHeader theme={theme}>
+          {getString('common.display')}
+        </List.SubHeader>
+        <SettingSwitch
+          label={getString('readerScreen.bottomSheet.fullscreen')}
+          value={fullScreenMode}
+          onPress={() =>
+            setChapterGeneralSettings({ fullScreenMode: !fullScreenMode })
+          }
+          theme={theme}
+        />
+        <SettingSwitch
+          label={getString('readerScreen.bottomSheet.showProgressPercentage')}
+          value={showScrollPercentage}
+          onPress={() =>
+            setChapterGeneralSettings({
+              showScrollPercentage: !showScrollPercentage,
+            })
+          }
+          theme={theme}
+        />
+        <SettingSwitch
+          label={getString('readerScreen.bottomSheet.showBatteryAndTime')}
+          value={showBatteryAndTime}
+          onPress={() =>
+            setChapterGeneralSettings({
+              showBatteryAndTime: !showBatteryAndTime,
+            })
+          }
+          theme={theme}
+        />
+        <SettingSwitch
+          label={getString('readerScreen.bottomSheet.keepScreenOn')}
+          value={keepScreenOn}
+          onPress={() =>
+            setChapterGeneralSettings({ keepScreenOn: !keepScreenOn })
+          }
+          theme={theme}
+        />
+      </View>
 
-        <View style={styles.section}>
-          <List.SubHeader theme={theme}>Reading Enhancements</List.SubHeader>
-          <SettingSwitch
-            label={getString('readerScreen.bottomSheet.bionicReading')}
-            value={bionicReading}
-            onPress={() =>
-              setChapterGeneralSettings({ bionicReading: !bionicReading })
-            }
-            theme={theme}
-          />
-        </View>
+      <View style={styles.section}>
+        <List.SubHeader theme={theme}>Reading Enhancements</List.SubHeader>
+        <SettingSwitch
+          label={getString('readerScreen.bottomSheet.bionicReading')}
+          value={bionicReading}
+          onPress={() =>
+            setChapterGeneralSettings({ bionicReading: !bionicReading })
+          }
+          theme={theme}
+        />
+      </View>
 
-        <View style={styles.bottomSpacing} />
-      </BottomSheetScrollView>
+      <View style={styles.bottomSpacing} />
+    </BottomSheetScrollView>
   );
 };
 
