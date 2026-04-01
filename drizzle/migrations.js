8c64465d34b278af6506512ee48b019fcdcbf017
CD-Z <69157453+CD-Z@users.noreply.github.com>
2026-04-01 16:11:24 +0200

    upgrade debs

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,10 +1,8 @@
-// This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
-
 import m0000 from './20251222152612_past_mandrill/migration.sql';
 
-export default {
-  journal: { entries: [] },
-  migrations: {
-    '20251222152612_past_mandrill': m0000,
-  },
-};
+  export default {
+    migrations: {
+      "20251222152612_past_mandrill": m0000
+}
+  }
+  
