1ec54a6ccf1a0ea6a02889cbe6dd8ee9bbbc3bf6
Palloxin <75091899+Palloxin@users.noreply.github.com>
2026-07-13 13:51:30 +0200

    lint f

--- android/app/src/main/assets/js/removeExtraParagraphSpacing.js
+++ android/app/src/main/assets/js/removeExtraParagraphSpacing.js
@@ -2,19 +2,19 @@ function removeExtraParagraphSpacing(html) {
   return html
     .replace(/(?:&nbsp;\s*|[\u200b]\s*)+(?=<\/?p[> ])/g, '')
         .replace(/<br>\s*<br>\s*(?:<br>\s*)+/g, '<br><br>') //force max 2 consecutive <br>, chaining regex
-        .replace(
-          /<br>\s*<br>[^]+/,
-          _ =>
-            `${
-              /\/p>/.test(_)
-                ? _.replace(
-                    /<br>\s*<br>(?:(?=\s*<\/?p[> ])|(?<=<\/?p(?:>| [^>]+>)<br>\s*<br>))\s*/g,
-                    '',
-                  )
-                : _
-            }`,
-        ) //if p found, delete all double br near p
-        .replace(/<br>(?:(?=\s*<\/?p[> ])|(?<=<\/?p(?:>| [^>]+>)(?:<[^>]+>)*\s*<br>))\s*/g, '');
+    .replace(
+       /<br>\s*<br>[^]+/,
+       _ =>
+        `${
+           /\/p>/.test(_)
+            ? _.replace(
+                 /<br>\s*<br>(?:(?=\s*<\/?p[> ])|(?<=<\/?p(?:>| [^>]+>)<br>\s*<br>))\s*/g,
+                '',
+               )
+            : _
+        }`,
+     ) //if p found, delete all double br near p
+     .replace(/<br>(?:(?=\s*<\/?p[> ])|(?<=<\/?p(?:>| [^>]+>)(?:<[^>]+>)*\s*<br>))\s*/g, '');
 }
 
 // WebView global
