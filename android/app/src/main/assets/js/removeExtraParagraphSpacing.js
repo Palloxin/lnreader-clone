b151f09bf1ddc7f853f8de147512a56635b66840
Palloxin <75091899+Palloxin@users.noreply.github.com>
2026-07-13 13:53:22 +0200

    Update removeExtraParagraphSpacing.js

--- android/app/src/main/assets/js/removeExtraParagraphSpacing.js
+++ android/app/src/main/assets/js/removeExtraParagraphSpacing.js
@@ -1,12 +1,12 @@
 function removeExtraParagraphSpacing(html) {
   return html
     .replace(/(?:&nbsp;\s*|[\u200b]\s*)+(?=<\/?p[> ])/g, '')
-        .replace(/<br>\s*<br>\s*(?:<br>\s*)+/g, '<br><br>') //force max 2 consecutive <br>, chaining regex
+    .replace(/<br>\s*<br>\s*(?:<br>\s*)+/g, '<br><br>') //force max 2 consecutive <br>, chaining regex
     .replace(
-       /<br>\s*<br>[^]+/,
-       _ =>
+     /<br>\s*<br>[^]+/,
+      _ =>
         `${
-           /\/p>/.test(_)
+         /\/p>/.test(_)
             ? _.replace(
                  /<br>\s*<br>(?:(?=\s*<\/?p[> ])|(?<=<\/?p(?:>| [^>]+>)<br>\s*<br>))\s*/g,
                 '',
