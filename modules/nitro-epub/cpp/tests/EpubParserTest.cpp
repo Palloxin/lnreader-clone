eef58f212f22b15e7871c1954467544181571016
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2026-08-17 21:16:29 +0530

    fix: Support Namespace-Prefixed EPUB Imports

--- modules/nitro-epub/cpp/tests/EpubParserTest.cpp
+++ modules/nitro-epub/cpp/tests/EpubParserTest.cpp
@@ -37,47 +37,48 @@ int main(int argc, char** argv) {
   writeFile(
       fixtureDirectory / "META-INF/container.xml",
       R"xml(<?xml version="1.0"?>
-<container>
-  <rootfiles>
-    <rootfile full-path="OEBPS/content.opf"/>
-  </rootfiles>
-</container>)xml");
+<ocf:container xmlns:ocf="urn:oasis:names:tc:opendocument:xmlns:container">
+  <ocf:rootfiles>
+    <ocf:rootfile full-path="OEBPS/content.opf"/>
+  </ocf:rootfiles>
+</ocf:container>)xml");
 
   writeFile(
       fixtureDirectory / "OEBPS/content.opf",
       R"xml(<?xml version="1.0"?>
-<package version="3.0">
-  <metadata>
+<opf:package xmlns:opf="http://www.idpf.org/2007/opf"
+             xmlns:dc="http://purl.org/dc/elements/1.1/" version="3.0">
+  <opf:metadata>
     <dc:title>Image fixture</dc:title>
-    <meta name="cover" content="cover-document"/>
-  </metadata>
-  <manifest>
-    <item id="nav" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
-    <item id="cover-document" href="Text/cover.xhtml" media-type="application/xhtml+xml"/>
-    <item id="chapter" href="Text/chapter.xhtml" media-type="application/xhtml+xml"/>
-    <item id="jpeg" href="Images/cover.jpg" media-type="image/jpeg"/>
-    <item id="png" href="Images/illustration.png" media-type="image/png"/>
-    <item id="webp" href="Images/illustration.webp" media-type="image/webp"/>
-    <item id="gif" href="Images/illustration.gif" media-type="image/gif"/>
-    <item id="svg" href="Images/illustration.svg" media-type="image/svg+xml"/>
-  </manifest>
-  <spine>
-    <itemref idref="cover-document"/>
-    <itemref idref="chapter"/>
-  </spine>
-</package>)xml");
+    <opf:meta name="cover" content="cover-document"/>
+  </opf:metadata>
+  <opf:manifest>
+    <opf:item id="nav" href="toc.ncx" media-type="application/x-dtbncx+xml"/>
+    <opf:item id="cover-document" href="Text/cover.xhtml" media-type="application/xhtml+xml"/>
+    <opf:item id="chapter" href="Text/chapter.xhtml" media-type="application/xhtml+xml"/>
+    <opf:item id="jpeg" href="Images/cover.jpg" media-type="image/jpeg"/>
+    <opf:item id="png" href="Images/illustration.png" media-type="image/png"/>
+    <opf:item id="webp" href="Images/illustration.webp" media-type="image/webp"/>
+    <opf:item id="gif" href="Images/illustration.gif" media-type="image/gif"/>
+    <opf:item id="svg" href="Images/illustration.svg" media-type="image/svg+xml"/>
+  </opf:manifest>
+  <opf:spine>
+    <opf:itemref idref="cover-document"/>
+    <opf:itemref idref="chapter"/>
+  </opf:spine>
+</opf:package>)xml");
 
   writeFile(
       fixtureDirectory / "OEBPS/toc.ncx",
       R"xml(<?xml version="1.0"?>
-<ncx>
-  <navMap>
-    <navPoint>
-      <navLabel><text>Chapter</text></navLabel>
-      <content src="Text/chapter.xhtml"/>
-    </navPoint>
-  </navMap>
-</ncx>)xml");
+<ncx:ncx xmlns:ncx="http://www.daisy.org/z3986/2005/ncx/">
+  <ncx:navMap>
+    <ncx:navPoint>
+      <ncx:navLabel><ncx:text>Chapter</ncx:text></ncx:navLabel>
+      <ncx:content src="Text/chapter.xhtml"/>
+    </ncx:navPoint>
+  </ncx:navMap>
+</ncx:ncx>)xml");
   writeFile(
       fixtureDirectory / "OEBPS/Text/chapter.xhtml",
       R"xml(<html><body>
@@ -91,6 +92,7 @@ int main(int argc, char** argv) {
   const EpubMetadata metadata = parseEpub(fixtureDirectory.string());
   const std::filesystem::path imageDirectory = fixtureDirectory / "OEBPS/Images";
 
+  assert(metadata.name == "Image fixture");
   assert(metadata.imagePaths.size() == 5);
   assert(containsPath(metadata.imagePaths, imageDirectory / "cover.jpg"));
   assert(containsPath(metadata.imagePaths,
@@ -105,6 +107,7 @@ int main(int argc, char** argv) {
   assert(metadata.chapters.size() == 2);
   assert(metadata.chapters.front().path ==
          (fixtureDirectory / "OEBPS/Text/cover.xhtml").string());
+  assert(metadata.chapters[1].name == "Chapter");
 
   const std::string coverChapter =
       readFile(fixtureDirectory / "OEBPS/Text/cover.xhtml");
