d4cc5f7fb14d1d5e8298ef9b3b675c6750517b1b
Palloxin <75091899+Palloxin@users.noreply.github.com>
2026-08-28 16:56:15 +0200

    Update SimpleCodeEditor.tsx

--- src/screens/settings/SettingsCustomCodeScreen/Components/SimpleCodeEditor.tsx
+++ src/screens/settings/SettingsCustomCodeScreen/Components/SimpleCodeEditor.tsx
@@ -286,7 +286,7 @@ export function useStableLineModels(value: string): LineModel[] {
 
   return useMemo(() => {
     const newLines = value
-      .replace(/\r\n|\r/g, '\n')
+      .replace(/\r\n?/g, '\n')
       .split('\n');
     const previous = previousRef.current;
 
